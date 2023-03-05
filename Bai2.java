17cabe9c9ee6afa4d016c6d2b2ad18b01acf9e4f
Ohamminh259 <mihphm9@gmail.com>
2023-03-05 13:47:15 +0700

    first commit

---
+++ Bai2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bai2;
+
+import java.util.Scanner;
+import java.util.Iterator;
+public class Bai2 {
+    public static void main (String [] args)
+    {
+        int n;
+        
+         
+         for (n=0;n<10;n++)
+         {
+             System.out.println(n+"Pham Minh");
+         }
+    }
+}
+        
+        
+
+
+
